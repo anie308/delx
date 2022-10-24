85fa9d98d320c8b86b5920ff4d86963a2a406abd
anie308 <aniebietabasiobo@gmail.com>
2022-10-24 23:00:40 +0100

    commit

--- src/components/homehero/index.jsx
+++ src/components/homehero/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeHero() {
   return (
-    <div className='h-full md:h-[400px] w-full gap-3 flex flex-col md:flex-row items-center justify-between p-[15px] md:p-[20px_40px]'>
+    <div className='bg-gradient-to-r from-yellow-50 to-yellow-200 h-full md:h-[400px] w-full gap-3 flex flex-col md:flex-row items-center justify-between p-[15px] md:p-[20px_40px]'>
       <div className='flex-1'>
         <p className='font-quicksand font-[900] text-[35px] md:text-[50px] leading-[50px] text-center md:text-start md:leading-[60px]'>Welcome to <br /><span className='text-secondary'> Delx Academy.</span></p>
         <p className='mt-[15px] font-quicksand font-[500] text-[16px] text-center md:text-start '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat veritatis molestiae sed excepturi nostrum. Facilis, nemo? Unde ullam repudiandae consequatur aliquam recusandae veniam neque illum exercitationem aperiam, est, repellat earum?</p>
