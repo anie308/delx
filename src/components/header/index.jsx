af913432833976762b0bdf716d12eb0ffa6c00a2
anie308 <aniebietabasiobo@gmail.com>
2022-10-25 17:34:05 +0100

    commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -27,16 +27,16 @@ function Navbar() {
     },
   ]
   return (
-    <div className=' md:top-0 z-10  md:p-[18px_40px] flex flex-col md:flex-row  items-center justify-between '>
-      <div className='flex p-[15px] md:p-0  items-center justify-between w-full md:w-fit'>
+    <div className=' lg:top-0 z-10  lg:p-[18px_40px] flex flex-col lg:flex-row  items-center justify-between '>
+      <div className='flex p-[15px] lg:p-0  items-center justify-between w-full lg:w-fit'>
         <div className='font-quicksand font-[900] text-[28px] cursor-pointer text-primary '><Link to='/'>Delx Academy</Link></div>
-        <div className='md:hidden  cursor-pointer' onClick={() => setToggle(!toggle)}>
+        <div className='lg:hidden  cursor-pointer' onClick={() => setToggle(!toggle)}>
           {toggle? <FaTimes className='text-[25px]'/> : <FaBars className='text-[25px]'/>}
         </div>
       </div>
-      <div className= {`${toggle? 'left-0' : 'left-[-100%] md:left-0'} transition-left absolute md:relative top-0 flex flex-col md:flex-row items-start md:items-center space-y-10 md:space-y-0 md:space-x-10 w-[220px] sm:w-[300px] md:w-fit h-full md:h-fit bg-gray-100 md:bg-transparent p-[20px] md:p-0`}>
-      <div className='flex md:hidden font-quicksand font-[900] text-[25px] cursor-pointer'>Delx Academy</div>
-          <div className='flex flex-col md:flex-row items-start md:items-center space-y-5 md:space-y-0 md:space-x-10'>
+      <div className= {`${toggle? 'left-0' : 'left-[-100%] lg:left-0'} transition-left absolute lg:relative top-0 flex flex-col lg:flex-row items-start lg:items-center space-y-10 lg:space-y-0 lg:space-x-10 w-[220px] sm:w-[300px] lg:w-fit h-full lg:h-fit bg-gray-100 lg:bg-transparent p-[20px] lg:p-0`}>
+      <div className='flex lg:hidden font-quicksand font-[900] text-[25px] cursor-pointer'>Delx Academy</div>
+          <div className='flex flex-col lg:flex-row items-start lg:items-center space-y-5 lg:space-y-0 lg:space-x-10'>
             {
               navLinks.map(({id, name, path})=> (
                 <Link to={path} key={id} className='text-[18px] font-[600] font-quicksand ' onClick={() => setToggle(!toggle)}>{name}</Link>
